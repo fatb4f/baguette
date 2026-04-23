1de5a1fbe9f422d96935cf1b64fc9d1568d5b2ce
_404 <fatb4f@gmail.com>
2026-04-23 11:05:17 -0400

    Fix artifact validation tar check

--- scripts/validate-artifacts.sh
+++ scripts/validate-artifacts.sh
@@ -8,12 +8,16 @@ ROOTFS_TAR_ZST="$OUTDIR/arch-rootfs.tar.zst"
 IMG="$OUTDIR/arch-baguette.img"
 IMG_ZST="$OUTDIR/arch-baguette.img.zst"
 
+ROOTFS_LIST="$(mktemp)"
+trap 'rm -f "$ROOTFS_LIST"' EXIT
+
 for path in "$ROOTFS_TAR" "$ROOTFS_TAR_ZST" "$IMG" "$IMG_ZST"; do
   test -f "$path"
 done
 
 zstd -t "$ROOTFS_TAR_ZST"
 zstd -t "$IMG_ZST"
+tar -tf "$ROOTFS_TAR" > "$ROOTFS_LIST"
 
 for path in \
   "etc/fstab" \
@@ -24,7 +28,7 @@ for path in \
   "etc/systemd/system/port-listener.service" \
   "usr/sbin/usermod"
 do
-  tar -tf "$ROOTFS_TAR" | grep -Fxq "./$path" || tar -tf "$ROOTFS_TAR" | grep -Fxq "$path"
+  grep -Fxq "./$path" "$ROOTFS_LIST" || grep -Fxq "$path" "$ROOTFS_LIST"
 done
 
 echo "Artifact validation passed"
